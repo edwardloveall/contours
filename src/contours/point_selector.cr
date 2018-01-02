c0636fb09effeca864f28ef5e85a406ee3993228
Edward Loveall <edward@edwardloveall.com>
2018-01-02 08:15:49 -0500

    Simple case of next_point working
    This is somewhat complicated. It creates an angle between the angle
    vector and candidate point. Then it calculates the angle between the
    two using the law of cosines. It then figures out what has the biggest
    angle and returns the associated point.

--- src/contours/point_selector.cr
+++ src/contours/point_selector.cr
@@ -8,4 +8,28 @@ class PointSelector
     @angle = 0.0
   )
   end
+
+  def next_point
+    largest_angle = 0
+    point = Point.zero
+    x = cos(PI - angle)
+    y = sin(angle)
+    origin = Point.new(x, y)
+    candidates.each do |candidate|
+      normalized_candidate = candidate - start_point
+      # make them into a triangle by adding the differece between them as the two sides
+      # use the law of cosines to get the cos of the angle (ratio) between the two vectors
+      dot = origin.dot(normalized_candidate)
+      product_magnitude = origin.magnitude * normalized_candidate.magnitude
+      angle_as_ratio = dot / product_magnitude
+      # use arc (inverse) cosine to get the angle in radians
+      radians = acos(angle_as_ratio)
+      if radians > largest_angle
+        largest_angle = radians
+        point = candidate
+      end
+    end
+
+    point
+  end
 end
