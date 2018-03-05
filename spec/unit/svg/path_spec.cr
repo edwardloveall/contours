f2f0188053f38fb2cab550c8325a98f45a927488
Edward Loveall <edward@edwardloveall.com>
2018-03-04 21:50:20 -0500

    Add points to path
    The tests had to change a bit because when I called `path` it would
    call the `path` method and create a new path every time. this caused
    confusion for a while as I tried to figure out why the path wasn't
    staying closed.

--- spec/unit/svg/path_spec.cr
+++ spec/unit/svg/path_spec.cr
@@ -6,9 +6,31 @@ describe SVG::Path do
       path.tag_name.should eq("path")
     end
   end
+
+  describe "#raw_path_instructions" do
+    it "returns the movment and line commands" do
+      path.raw_path_instructions.should eq("M10 10 L 90 10 L 90 90 L 10 90")
+    end
+
+    context "when the path is closed" do
+      it "returns the movment, line, and close commands" do
+        saved_path = path
+
+        saved_path.close!
+
+        saved_path.raw_path_instructions.should eq("M10 10 L 90 10 L 90 90 L 10 90 Z")
+      end
+    end
+  end
 end
 
 private def path
   SVG::Path.new.tap do |path|
+    path.points = [
+      CrystalEdge::Vector2.new(x: 10.0, y: 10.0),
+      CrystalEdge::Vector2.new(x: 90.0, y: 10.0),
+      CrystalEdge::Vector2.new(x: 90.0, y: 90.0),
+      CrystalEdge::Vector2.new(x: 10.0, y: 90.0)
+    ]
   end
 end
--- src/svg/path.cr
+++ src/svg/path.cr
@@ -1,5 +1,27 @@
 class SVG::Path
+  property :points
+  @points = Array(CrystalEdge::Vector2).new
+  @closed = false
+
   def tag_name
     "path"
+  end
+
+  def raw_path_instructions
+    string = "M"
+    point_string = points.map do |point|
+      [point.x, point.y].map(&.to_i).join(" ")
+    end
+    string += point_string.join(" L ")
+    string += " Z" if closed?
+    string
+  end
+
+  def close!
+    @closed = true
+  end
+
+  def closed?
+    @closed
   end
 end
