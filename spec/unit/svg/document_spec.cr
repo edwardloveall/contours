929c4c319f7dab46f48811baa3d4c471e48d8bb2
Edward Loveall <edward@edwardloveall.com>
2018-03-08 12:56:42 -0500

    Add basic SVG::Document class
    This prints a string of a valid SVG document.

---
+++ spec/unit/svg/document_spec.cr
@@ -0,0 +1,38 @@
+require "../../spec_helper"
+
+describe SVG::Document do
+  describe "#text_content" do
+    it "returns text representing an SVG file" do
+      document.text_content.should eq <<-SVG
+      <?xml version="1.0"?>
+      <svg width="100" height="100" xmlns="http://www.w3.org/2000/svg"/>
+
+      SVG
+    end
+
+    context "when the svg has elements" do
+      it "returns text representing an SVG file" do
+        svg = document
+        path = SVG::Path.new
+        path.points = [
+          CrystalEdge::Vector2.new(x: 20.0, y: 0.0),
+          CrystalEdge::Vector2.new(x: 0.0, y: 30.0)
+        ]
+        path.close!
+        svg.elements << path
+
+        svg.text_content.should eq <<-SVG
+        <?xml version="1.0"?>
+        <svg width="100" height="100" xmlns="http://www.w3.org/2000/svg">
+          <path d="M20 0 L 0 30 Z"/>
+        </svg>
+
+        SVG
+      end
+    end
+  end
+end
+
+private def document
+  SVG::Document.new(width: 100, height: 100)
+end
---
+++ src/svg/document.cr
@@ -0,0 +1,21 @@
+require "xml"
+
+class SVG::Document
+  property :elements, :width, :height
+  @elements = Array(SVG::Path).new
+
+  NAMESPACE = "http://www.w3.org/2000/svg"
+
+  def initialize(@width = 100, @height = 100)
+  end
+
+  def text_content
+    XML.build(indent: "  ") do |xml|
+      xml.element("svg", width: width, height: height, xmlns: NAMESPACE) do
+        elements.each do |element|
+          xml.element(element.tag_name, element.tag_attributes)
+        end
+      end
+    end
+  end
+end
