21f3ca8d1492e7021737ce9c1471a2cb5650086c
Edward Loveall <edward@edwardloveall.com>
2018-03-14 16:56:57 -0400

    Find the point with the smallest angle from the origin
    Imagine a bunch of points. Your goal is to find the point that is the
    least angle from the x-axis. That's what this class does right now.
    
    Next up, I'll be making it work if your starting angle is not 0 radians
    (x-axis) but any arbitrary angle.

--- spec/unit/smallest_angle_spec.cr
+++ spec/unit/smallest_angle_spec.cr
@@ -23,4 +23,27 @@ describe Contours::SmallestAngle do
       result.should eq(adjusted)
     end
   end
+
+  describe "#find_point" do
+    context "when the offset angle is 0 radians" do
+      it "finds the point with the smallest clockwise angle" do
+        target = Point.new(7.0, 4.0)
+        points = [
+          Point.new(3.0, 0.0),
+          Point.new(6.0, -1.0),
+          Point.new(4.0, 8.0),
+          target,
+          Point.new(0.0, -1.0),
+          Point.new(-2.0, 7.0),
+          Point.new(0.0, 5.0)
+        ]
+        origin = Point.new(3.0, 3.0)
+        smallest = Contours::SmallestAngle.new(origin: origin, points: points)
+
+        result = smallest.find_point
+
+        result.should eq(target)
+      end
+    end
+  end
 end
--- src/contours/smallest_angle.cr
+++ src/contours/smallest_angle.cr
@@ -7,4 +7,11 @@ class Contours::SmallestAngle
   def biased_points
     @points.map { |point| point - @origin }
   end
+
+  def find_point
+    @points.sort_by do |point|
+      biased_point = point - @origin
+      (biased_point.angle + TWO_PI) % TWO_PI
+    end.first
+  end
 end
