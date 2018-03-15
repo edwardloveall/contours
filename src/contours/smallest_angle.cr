2fdf98e219b6f3ac2f092a58157090ebeb71d3c4
Edward Loveall <edward@edwardloveall.com>
2018-03-15 13:28:45 -0400

    Remove biased points from SmallestAngle
    This is taken care of by the `angle_to` method usedwhen sorting the
    points.

--- src/contours/smallest_angle.cr
+++ src/contours/smallest_angle.cr
@@ -4,10 +4,6 @@ class Contours::SmallestAngle
   def initialize(@origin : Point, @points : Array(Point), @offset_angle = 0.0)
   end
 
-  def biased_points
-    @points.map { |point| point - @origin }
-  end
-
   def find_point
     @points.sort_by do |point|
       (@origin.angle_to(point) - @offset_angle + TWO_PI) % TWO_PI
