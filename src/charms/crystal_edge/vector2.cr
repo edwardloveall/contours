1941256c0415d6aa2ad5bd4e2e1e32feed321b2c
Edward Loveall <edward@edwardloveall.com>
2018-04-27 15:44:26 -0400

    Add debugging methods to Vector2

--- src/charms/crystal_edge/vector2.cr
+++ src/charms/crystal_edge/vector2.cr
@@ -4,6 +4,14 @@ struct CrystalEdge::Vector2
     @y = y.to_f
   end
 
+  def to_s(io)
+    io.write_utf8("(#{@x}, #{@y})".to_slice)
+  end
+
+  def inspect
+    "(#{@x}, #{@y})"
+  end
+
   def angle_to(other : Vector2)
     (other - self).angle
   end
