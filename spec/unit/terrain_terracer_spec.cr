96e759a9637a748d2dd765cb23ef3f1ba0f8fd56
Edward Loveall <edward@edwardloveall.com>
2018-05-26 15:25:54 -0400

    Use Vector3 instead of HeightPoint

--- spec/unit/terrain_terracer_spec.cr
+++ spec/unit/terrain_terracer_spec.cr
@@ -3,31 +3,30 @@ require "../spec_helper"
 describe Contours::TerrainTerracer do
   context "when there are no points" do
     it "returns an empty array" do
-      levels = Contours::TerrainTerracer.terrace([] of HeightPoint)
+      levels = Contours::TerrainTerracer.terrace([] of Point)
 
-      levels.should eq([] of HeightPoint)
+      levels.should eq([] of Point)
     end
   end
 
   it "returns groups of points separated into their heigh tiers" do
-    dummy_point = Point.new(1, 1)
     heights = [98, 1, 42, 43, 99]
     points = heights.map do |height|
-      HeightPoint.new(height: height, point: dummy_point)
+      Point.new(z: height, x: 1, y: 1)
     end
 
     levels = Contours::TerrainTerracer.terrace(points, step: 10)
 
     levels[0].should contain_exactly([
-      HeightPoint.new(height: 1, point: dummy_point)
+      Point.new(z: 1, x: 1, y: 1)
     ])
     levels[4].should contain_exactly([
-      HeightPoint.new(height: 42, point: dummy_point),
-      HeightPoint.new(height: 43, point: dummy_point)
+      Point.new(z: 42, x: 1, y: 1),
+      Point.new(z: 43, x: 1, y: 1)
     ])
     levels[9].should contain_exactly([
-      HeightPoint.new(height: 98, point: dummy_point),
-      HeightPoint.new(height: 99, point: dummy_point)
+      Point.new(z: 98, x: 1, y: 1),
+      Point.new(z: 99, x: 1, y: 1)
     ])
   end
 end
--- src/contours/terrain_terracer.cr
+++ src/contours/terrain_terracer.cr
@@ -1,16 +1,16 @@
 class Contours::TerrainTerracer
-  def self.terrace(points : Array(HeightPoint), step : Int32 = 10)
+  def self.terrace(points : Array(Point), step : Int32 = 10)
     new.terrace(points, step)
   end
 
-  def terrace(points : Array(HeightPoint), step : Int32 = 10)
-    levels = [] of Array(HeightPoint)
+  def terrace(points : Array(Point), step : Int32 = 10)
+    levels = [] of Array(Point)
     return levels if points.empty?
-    start = points.min_of { |point| point.height }
+    start = points.min_of { |point| point.z }
     current_level = start + step
 
     while !points.empty?
-      level_points = points.select { |point| point.height < current_level }
+      level_points = points.select { |point| point.z < current_level }
       points = points - level_points
       levels.push(level_points)
       current_level += step
